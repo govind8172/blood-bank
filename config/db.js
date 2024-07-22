26f2cc21db526ad48161beb640dd772e93bcb3ba
Govind Kumar <govindkr992001@gmail.com>
2024-07-22 14:54:42 +0530

    first

--- config/db.js
+++ config/db.js
@@ -1,12 +1,30 @@
-const mongoose=require ('mongoose')
- const connectDB= async()=>{
-    try{
-        await mongoose.connect(process.env.MONGO_URL)
-        console.log(`Connected to mongo db successfully${mongoose.connection.host}`)
+// const mongoose=require ('mongoose')
+//  const connectDB= async()=>{
+//     try{
+//         await mongoose.connect(process.env.MONGO_URL)
+//         console.log(`Connected to mongo db successfully${mongoose.connection.host}`)
 
+//     }
+//     catch(error){
+//         console.log(`Mongodb connection failed ${error}`)
+//     }
+//  }
+//  module.exports=connectDB
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true,
+        });
+        console.log(`Connected to MongoDB successfully: ${conn.connection.host}`);
+    } catch (error) {
+        console.log(`MongoDB connection failed: ${error.message}`);
+        process.exit(1); // Exit process with failure
     }
-    catch(error){
-        console.log(`Mongodb connection failed ${error}`)
-    }
- }
- module.exports=connectDB
+};
+
+module.exports = connectDB;
